8e74112e88d0b194a595deec5a038cd5640f10ab
Cory Washington <tbd>
2023-04-15 11:40:36 -0400

    Code for Project Euler #44

---
+++ 44/project-euler.44.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# Imports
+
+# Functions
+def pentagonalNumber(n):
+    return n*(3*n-1)//2
+
+# Main logic
+PRECOMPUTE_LEN = 10000
+pentagonalNumbers = [pentagonalNumber(i) for i in range(1, PRECOMPUTE_LEN+1)]
+asSet = set(pentagonalNumbers)
+
+validPairs = []
+for i in range(1, PRECOMPUTE_LEN):
+    for j in range(i+1, PRECOMPUTE_LEN):
+        iValue = pentagonalNumbers[i]
+        jValue = pentagonalNumbers[j]
+        s = jValue + iValue
+        d = jValue - iValue
+        if ((s in asSet) and (d in asSet)):
+            validPair = (i, j)
+            validPairValues = (iValue, jValue)
+            validPairs.append(validPair)
+            print(f"{validPair} -> {validPairValues} -> {d}")
+
