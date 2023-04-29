056db4261e6b9654a07dc8904ccdea8ab28564e6
Cory Washington <tbd>
2023-04-28 23:21:17 -0400

    Code for Project Euler #2

---
+++ 2/project-euler.2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# Imports
+
+# Constants
+N = 4 * 1000 * 1000
+
+# Functions
+
+# Main logic
+def main():
+    a = 1
+    b = 1
+    s = 0
+    while b < N:
+        if b % 2 == 0:
+            print(b)
+            s += b
+        tmp = b
+        b += a
+        a = tmp
+    print(s)
+
+if __name__ == "__main__":
+    main()
