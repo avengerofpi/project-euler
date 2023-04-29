759a499c8b68b16f53bdbd1a2ac1a3c0e9c2471f
Cory Washington <tbd>
2023-04-28 23:13:21 -0400

    Code for Project Euler #1

---
+++ 1/project-euler.1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# Imports
+
+# Constants
+N = 1000
+a = 3
+b = 5
+
+# Functions
+
+# Main logic
+def main():
+    #s = sum(i for i in range(N) if ((i % a == 0) or (i % b == 0)))
+    s = sum(range(0,N,a)) + sum(range(0,N,b)) - sum(range(0,N,a*b))
+    print(s)
+
+if __name__ == "__main__":
+    main()
