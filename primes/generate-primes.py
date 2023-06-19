a051da32d91748290066bce6aaea9bf7c2329a76
Cory Washington <tbd>
2023-06-19 19:36:42 -0400

    547: add candidate test cases for N=5..23 (values taken from current, slow logic)

--- primes/generate-primes.py
+++ primes/generate-primes.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
 
 # Imports
+from datetime import timedelta
+import time
 
 # Constants
 N = 1 * 1000 * 1000 * 1000
 
+# Logging
+info = False
+debug = True
+verbose = False
+def logInfo(msg = ""):
+    if info:
+        print(msg, flush=True)
+def logDebug(msg = ""):
+    if debug:
+        print(msg, flush=True)
+def logVerbose(msg = ""):
+    if verbose:
+        print(msg, flush=True)
+
 # Functions
+def getTimeInMillis():
+    return int(time.time() * 1000)
+
 def computePrimesUpToN(N):
     """
     Inefficiently determine all primes up to (and including) the input
@@ -22,12 +41,19 @@ def computePrimesUpToN(N):
                 checkPrimesI += 1
             if all(candidateP % p != 0 for p in primes[0:checkPrimesI + 1]):
                 primes.append(candidateP)
-                print(f"{candidateP}")
+                logInfo(f"{candidateP}")
     return tuple(primes)
 
 # Main logic
 def main():
-    computePrimesUpToN(N)
+    powersOf10 = [4, 5, 6, 7, 8]
+    for i in powersOf10:
+        N = 10 ** i
+        startTime = getTimeInMillis()
+        primes = computePrimesUpToN(N)
+        endTime = getTimeInMillis()
+        logTimeDiff = endTime - startTime
+        logDebug(f"Time spent computing {len(primes):9} <= {N:12} = 10 ** {i:2} -> {timedelta(milliseconds=logTimeDiff)}")
 
 if __name__ == "__main__":
     main()
