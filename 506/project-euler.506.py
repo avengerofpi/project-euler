f199c85665e857752e4a82584e88d22d1d2a07ad
Cory Washington <tbd>
2023-06-23 20:47:43 -0400

    506: use geometric series to simplify some somes; more test cases; faster

--- 506/project-euler.506.py
+++ 506/project-euler.506.py
@@ -6,6 +6,8 @@ from collections import defaultdict
 import scipy
 import numpy
 from numpy import array
+from datetime import timedelta
+import time
 
 # Tests
 class TestCase:
@@ -13,8 +15,12 @@ class TestCase:
         self.N = N
         self.expected = expected
 TESTS = [
-    TestCase(11, 36120),
-    TestCase(1000, 18232686),
+    TestCase(        11,     36120),
+    TestCase(       100,   9291482),
+    TestCase(      1000,  18232686),
+    TestCase(      2000,  65456260),
+    TestCase(   10 ** 4, 107644726),
+    TestCase(2* 10 ** 4,   6116055),
     #TestCase(10 ** 8, "UNKNOWN"),
 ]
 
@@ -49,10 +55,10 @@ clockLen = len(clockSpokes)
 clockSpokeSum = sum(clockSpokes)
 MODULUS = 123454321
 
-# Logging 
+# Logging
 info = True
-debug = False
-verbose = True
+debug = True
+verbose = False
 def logInfo(msg = ""):
     if info:
         print(msg, flush=True)
@@ -63,7 +69,19 @@ def logVerbose(msg = ""):
     if verbose:
         print(msg, flush=True)
 
+def getTimeInMillis():
+    return int(time.time() * 1000)
+
 # Functions
+memoExponents = range(10)
+memoModPowersOfTen = { i: 10**i % MODULUS for i in memoExponents }
+def modPowersOfTen(n):
+    try:
+        return memoModPowersOfTen[n]
+    except:
+        m = min(i for i in memoModPowersOfTen.keys() if i < n)
+        return (memoModPowersOfTen[m] * 10**(n-m)) % MODULUS
+
 def S(N):
     logDebug(f"Computing S({N})")
     total = 0
@@ -72,11 +90,16 @@ def S(N):
     else:
         k = N // clockLen
         total += clockSpokeSum * k
-        total += sum(clockWheel[a] * sum(sum(10**(6*j) for j in range(i)) for i in range(k)) * (10**clockSpokeSizes[a]) for a in range(clockLen))
+        for a in range(clockLen):
+            for i in range(k):
+                inc = clockWheel[a] * ((10**(6*i) - 1) // (10**6 - 1) ) * modPowersOfTen(clockSpokeSizes[a])
+                total = (inc + total) % MODULUS
 
         q = N % clockLen
         total += sum(clockSpokes[0:q])
-        total += sum(clockWheel[a] * sum(10**(6*i) for i in range(k)) * (10**clockSpokeSizes[a]) for a in range(q))
+        for a in range(q):
+            inc = clockWheel[a] * ((10**(6*k) - 1) // (10**6 - 1) ) * modPowersOfTen(clockSpokeSizes[a])
+            total = (inc + total) % MODULUS
         logVerbose(f"  k = {k}, q = {q}")
 
     logDebug(f"Total for N={N}: {total}")
@@ -87,13 +110,18 @@ def SwithMod(N):
 
 def runTests():
     for test in TESTS:
+        startTime = getTimeInMillis()
         N = test.N
         expected = test.expected
-        logInfo(f"Running against N = {N}", flush=True)
+        logInfo(f"Running against N = {N}")
 
         ans = SwithMod(N)
         successStr = "SUCCESS" if (ans == expected) else f"FAILURE (expected {expected})"
         logInfo(f"{N}: {ans} - {successStr}")
+
+        endTime = getTimeInMillis()
+        logTimeDiff = endTime - startTime
+        logDebug(f"  Time spent: {timedelta(milliseconds=logTimeDiff)}")
         logInfo()
 
 def troubleshoot():
